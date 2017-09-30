e9b72094dd4914419eb7cd78e0814009ec7fac92
tylerkontra <tkontra551@gmail.com>
2017-09-29 22:39:50 -0700

    Continue devel filetypes parser

--- pysorter/filetypes_parser.py
+++ pysorter/filetypes_parser.py
@@ -6,23 +6,31 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 def parse_filetypes_yaml(filetypes_path):
     with open(filetypes_path, 'r') as file:
-        filetypes_data = yaml.load(file)
-
+        filetypes_data = yaml.load(file) # yaml to dict
+    pp.pprint(filetypes_data)
+    # separate config from rules
     config = filetypes_data['config']
     rules = filetypes_data['rules']
 
+    # placeholder skip list if not already present
+    if not rules['skips']:
+        rules['skips'] = []
+
     rule_groups = {}
     for rule_name in rules:
-        if 'patterns' in rules[rule_name]:
+        if 'patterns' in rules[rule_name]: # patterns key indicates a group
             if 'gpri' not in rules[rule_name]:
-                rules[rule_name]['gpri'] = 0
-            rule_groups[rule_name] = rules[rule_name]
-            '''
-            if 'skips' in rules[rule_name]:
-                rules['skips'].extend(rules[rule_name]['skips'])
-                rules[rule_name].pop('skips')
-            '''
+                rules[rule_name]['gpri'] = 0 # implicit group priority of 0
+            rule_groups[rule_name] = rules[rule_name] # add current group to rule_groups
             print('Group:', rule_name, '\n', rules[rule_name])
+
+        if 'skips' in rules[rule_name]:
+            rules['skips'].extend(rules[rule_name]['skips']) # extend
+            rules[rule_name].pop('skips')
+
+        else:
+            print("Rule:", rule_name, '\n', rules[rule_name])
+
         if rule_name == 'skips':
             skips = []
             for subitem in rules['skips']:
